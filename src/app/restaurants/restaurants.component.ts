0df4ef7dd28148c8f67fb8f33473214966a42296
Your Name <you@example.com>
2022-06-12 22:56:25 -0300

    import nao uilizado

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,7 +1,7 @@
 import { RestaurantsService } from "./restaurants.service";
 import { Component, OnInit } from "@angular/core";
 import { Restaurant } from "./restaurant/restaurant.model";
-import { RestaurantComponent } from "./restaurant/restaurant.component";
+
 @Component({
   selector: "mt-restaurants",
   templateUrl: "./restaurants.component.html",

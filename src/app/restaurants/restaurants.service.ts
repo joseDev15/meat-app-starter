1b1c3ec3a9acda828607f78ef87aeb49130e5995
Your Name <you@example.com>
2022-06-13 12:42:52 -0300

    tratamentos de erros http rest

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,16 +1,19 @@
+import { ErrorHandler } from "./../app.error-handler";
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 import { Restaurant } from "./restaurant/restaurant.model";
-import 'rxjs/add/operator/map'
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 @Injectable()
 export class RestaurantsService {
   constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
     return this.http
-      .get(`${ MEAT_API }/restaurants`)
-      .map((response) => response.json());
+      .get(`${MEAT_API}/restaurants`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.errorHandler);
   }
 }

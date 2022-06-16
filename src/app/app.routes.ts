7801305e3125171f7364e5e23adee1d1aa532d48
Your Name <you@example.com>
2022-06-16 19:12:21 -0300

    48 - rotas filhas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { AboutComponent } from './about/about.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { Route, Routes } from "@angular/router";
 
-export const ROUTES: Routes =
-[
+export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "restaurants", component: RestaurantsComponent },
-  { path: 'restaurants/:id', component: RestaurantDetailComponent }
-
+  {
+    path: "restaurants/:id",
+    component: RestaurantDetailComponent,
+    children: [
+      {path: "" ,redirectTo:'menu',pathMatch:'full'},
+      {path: "reviews" ,component: ReviewsComponent},
+      {path: "menu" ,component: MenuComponent},
+    ],
+  },
 ];
-  

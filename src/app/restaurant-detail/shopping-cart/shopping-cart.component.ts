b567411c6b0be7fbdd5a8b2f5ed3af628a87ce9f
Your Name <you@example.com>
2022-06-19 15:27:36 -0300

    52 - implementado carrinhos de compras

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from "./shopping-cart.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  selector: "mt-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.css"],
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor(private ShoppingCartService: ShoppingCartService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  items(): any[] {
+    let itens = this.ShoppingCartService.items;
+    console.log(
+      "🚀 ~ file: shopping-cart.component.ts ~ line 17 ~ ShoppingCartComponent ~ items ~ itens",
+      itens
+    );
+    return itens;
   }
+  total(): number {
+    let total = this.ShoppingCartService.total();
+    console.log("🚀 ~ file: shopping-cart.component.ts ~ line 23 ~ ShoppingCartComponent ~ total ~ total", total)
 
+    return total;
+  }
 }
